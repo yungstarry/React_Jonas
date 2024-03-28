b8d7732e78307685bc8bf26a3b200fa8db3f79b2
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-03-28 18:51:42 +0100

    redux

---
+++ 15-redux-intro/starter/src/features/accounts/AccountSlice.js
@@ -0,0 +1,37 @@
+import { combineReducers, createStore } from "redux";
+
+const initialStateAcount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function acountReducer(state = initialStateAcount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        purpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+      break;
+  }
+}
+
+
+export {acountReducer}
---
+++ 15-redux-intro/starter/src/features/customers/CustomerSlice.js
@@ -0,0 +1,26 @@
+import { combineReducers, createStore } from "redux";
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+export {customerReducer}
