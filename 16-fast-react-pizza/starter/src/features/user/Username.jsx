e728d2a36ec01140dea82d189cda126c3f03bfe1
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-04-03 00:55:58 +0100

    tailwind

--- 16-fast-react-pizza/starter/src/features/user/Username.jsx
+++ 16-fast-react-pizza/starter/src/features/user/Username.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Username = () => {
-  return <div className="hidden text-sm font-semibold md:block ">Username</div>;
+   const {username}=  useSelector((state) => state.user)
+   console.log(username);
+  return <div className="hidden text-sm font-semibold md:block ">{username || "UserName"}</div>;
 };
 
 export default Username;
+
+
+
