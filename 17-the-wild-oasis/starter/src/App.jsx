a098b343b3328879dd87248ba31bbd7b617d7153
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-05-11 21:57:47 +0100

    style finished

--- 17-the-wild-oasis/starter/src/App.jsx
+++ 17-the-wild-oasis/starter/src/App.jsx
@@ -1,25 +1,34 @@
-import styled from "styled-components"
-import GlobalStyles from "./styles/GlobalStyles"
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-
-
-
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const App = () => {
   return (
     <>
-    <GlobalStyles />
-    <div>
-
-    <Heading>App</Heading>
-    <Button>star</Button>
-
-    <Input type="number" name="" placeholder="number of quest" />
-    </div>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- 17-the-wild-oasis/starter/src/ui/Heading.jsx
+++ 17-the-wild-oasis/starter/src/ui/Heading.jsx
@@ -1,11 +1,26 @@
-import styled from "styled-components";
-
+import styled, { css } from "styled-components";
 
 
 const Heading = styled.h1`
-  font-size: 50px;
-  font-weight: 600;
-  background-color: yellow;
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+  ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 1rem;
+      font-weight: 500;
+    `}
+    line-height: 1.4;
 `;
 
 export default Heading
