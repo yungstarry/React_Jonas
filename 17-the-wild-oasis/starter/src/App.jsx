5cbc9d3c2291a853e0e8f3fade158107b35e18a9
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-05-11 20:50:33 +0100

    wild oasis

--- 17-the-wild-oasis/starter/src/App.jsx
+++ 17-the-wild-oasis/starter/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <GlobalStyles />
+    <div>
+
+    <Heading>App</Heading>
+    <Button>star</Button>
+
+    <Input type="number" name="" placeholder="number of quest" />
+    </div>
+    </>
   )
 }
 
---
+++ 17-the-wild-oasis/starter/src/ui/Heading.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+
+const Heading = styled.h1`
+  font-size: 50px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+export default Heading
