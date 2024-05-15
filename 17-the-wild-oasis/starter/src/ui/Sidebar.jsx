17a6678b6a2db4bce9166ee98149629a32e0b44a
yungstarry <111007711+yungstarry@users.noreply.github.com>
2024-05-15 01:32:00 +0100

    final style

---
+++ 17-the-wild-oasis/starter/src/ui/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components';
+import Logo from './Logo';
+import MainNav from './MainNav';
+
+
+const StyledSiderbar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-bottom: 1px solid var(--color-grey-100);
+  grid-row: 1/-1; 
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+
+`;
+
+const Sidebar = () => {
+  return (
+    <StyledSiderbar>
+        <Logo />
+        <MainNav />
+    </StyledSiderbar>
+  )
+}
+
+export default Sidebar
